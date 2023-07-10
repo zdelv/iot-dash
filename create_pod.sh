a3c5c773ba341f9ffef713f22b52831f0dcbe7bb
Zach <zachdelvecchio@gmail.com>
2023-07-09 21:23:23 -0400

    Add create_pod.sh
    Previously, either a kubernetes yaml config file or a docker-compose
    config file were used to setup the podman containers. These two methods,
    while good on paper, have issues regarding setup and cohesion with
    podman. Podman advertises compatibility with Docker, and, for the most
    part, it does seem to meet that, but it lacks compatibility with Docker
    Compose. The previous docker compose support uses a 3rd party tool
    (written in Python) to "automatically" build pods from docker-compose
    scripts. Unfortunately, podman does not contain the same level of
    features that a docker compose script expects, so not all features work
    (namely depends-on).
    
    The kubernetes file was supposed to be a work-around for docker-compose
    issues. Podman advertises limited kubernetes capabilities (and uses
    similar vocabulary (pods)). The kubernetes does work to start up pods,
    but it's somewhat awkward to edit and doesn't really fit the podman
    ecosystem still.
    
    As a last resort (and probably what should have been the first attempt),
    I decided to use the supported method of setting up podman pods, which
    is the CLI. create_pod.sh is a script that automatically sets up all
    containers and adds them to a pod. Users can then use `podman pod start
    <pod_name>` to start the pod. The create_pod.sh script is invoked with
    the name of the pod (e.g., create_pod.sh <pod_name>).
    
    The ingest/.env file was also modified to support these changes.

---
+++ create_pod.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+POD_NAME=$1
+NETWORK_NAME=${POD_NAME}_network
+VOLUME_NAME=${POD_NAME}_volume
+
+if [[ $POD_NAME == "" ]]; then
+	echo "Please supply a name for the pod."
+	exit
+fi
+
+
+# Create the pod, network, and volume if they do not already exist.
+podman pod inspect $POD_NAME || podman pod create $POD_NAME
+podman network inspect $NETWORK_NAME || podman network create $NETWORK_NAME
+podman volume inspect $VOLUME_NAME || podman volume create $VOLUME_NAME
+
+# MQTT
+podman create \
+	--pod $POD_NAME \
+	--net $NETWORK_NAME \
+	--network-alias mqtt \
+	--name mqtt \
+	-p 1883:1883 \
+	--volume ./configs/mosquitto.conf:/mosquitto/config/mosquitto.conf \
+	docker.io/eclipse-mosquitto
+
+# PostgreSQL
+podman create \
+	--pod $POD_NAME \
+	--net $NETWORK_NAME \
+	--network-alias db \
+	--name db \
+	-p 5432:5432 \
+	-e POSTGRES_PASSWORD_FILE=/run/secrets/db_password \
+	--volume ./configs/db_password.txt:/run/secrets/db_password \
+	--volume ./configs/db:/docker-entrypoint-initdb.d \
+	docker.io/postgres
+
+# Adminer
+podman create \
+	--pod $POD_NAME \
+	--net $NETWORK_NAME \
+	--network-alias adminer \
+	--name adminer \
+	-p 8081:8080 \
+	docker.io/adminer
+
+# If the ingest image isn't already created, then create it.
+if [[ $(podman inspect ingest) == "[]" ]]; then
+	echo "Could not find the ingest image. Building it now."
+	cd ingest
+	podman build -t ingest .
+fi
+
+# Ingest
+podman create \
+	--pod $POD_NAME \
+	--net $NETWORK_NAME \
+	--network-alias ingest \
+	--name ingest \
+	--restart on-failure \
+	--volume ./ingest/.env:/.env \
+	ingest \
+	ingest
