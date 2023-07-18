2d566f8dd1c16d37cb5bbe8562ee8de07272cbf3
Zach DelVecchio <zachdelvecchio@gmail.com>
2023-07-17 22:06:40 -0400

    Update create_pod.sh to add db-api and logs
    db-api is now required to run the entire platform. This adds db-api to
    the create_pod.sh script, meaning it now exists in the pod.
    
    Logging was also added to ingest and db-api, so this adds support for
    the volume mounts and the env variables. Also adds an if-statement to
    create_pod.sh that creates the pod_logs directory if it doesn't already
    exist.

--- create_pod.sh
+++ create_pod.sh
@@ -51,6 +51,7 @@ if [[ $(podman inspect ingest) == "[]" ]]; then
 	echo "Could not find the ingest image. Building it now."
 	cd ingest
 	podman build -t ingest ../ --file Dockerfile
+	cd ..
 fi
 
 # Ingest
@@ -61,7 +62,37 @@ podman create \
 	--name ingest \
 	--restart on-failure \
 	-e CONFIG=config.yaml \
+	-e POD=true \
+	-e LOG_DIR=/logs \
 	--volume ./ingest/.env:/.env \
 	--volume ./ingest/config.yaml:/config.yaml \
+	--volume ./pod_logs:/logs \
 	ingest \
 	ingest
+
+# If the db-api image isn't already created, then create it.
+if [[ $(podman inspect db-api) == "[]" ]]; then
+	echo "Could not find the db-api image. Building it now."
+	cd db-api
+	podman build -t db-api ../ --file Dockerfile
+	cd ..
+fi
+
+# db-api
+podman create \
+	--pod $POD_NAME \
+	--net $NETWORK_NAME \
+	--network-alias db-api \
+	--name db-api \
+	--restart on-failure \
+	-p 3001:3001 \
+	-e POD=true \
+	-e LOG_DIR=/logs \
+	--volume ./db-api/.env:/.env \
+	--volume ./pod_logs:/logs \
+	db-api \
+	db-api
+
+if [[ ! -d pod_logs ]]; then
+	mkdir pod_logs
+fi
